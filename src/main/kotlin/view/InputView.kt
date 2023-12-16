1f40c2ea5d6c07da3cb6c933d07514049e381c2f
gaeun <geun5744@gmail.com>
2023-12-16 13:09:17 +0900

    푸쉬 테스트

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -6,27 +6,17 @@ import model.Validation
 class InputView {
 
     fun readNumbers(): List<Int> {
-        return try {
-            print("숫자를 입력해주세요 : ")
-            val input = Console.readLine()
-            Validation.checkBaseballNum(input)
-            input.toList().map { it.toString().toInt() }
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            readNumbers()
-        }
+        print("숫자를 입력해주세요 : ")
+        val input = Console.readLine()
+        Validation.checkBaseballNum(input)
+        return input.toList().map { it.toString().toInt() }
     }
 
     fun readIsRetry(): Boolean {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        return try {
-            val input = Console.readLine()
-            Validation.checkRetry(input)
-            isRetryIntToBoolean(input)
-        } catch (e: IllegalArgumentException) {
-            println(e.message)
-            readIsRetry()
-        }
+        val input = Console.readLine()
+        Validation.checkRetry(input)
+        return isRetryIntToBoolean(input)
     }
 
     private fun isRetryIntToBoolean(input: String): Boolean = input == "1"
